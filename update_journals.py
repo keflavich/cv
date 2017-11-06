a2b397b9b992d6278503e63eb5102f78a5e7d8e7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-11-06 14:45:36 -0700

    cv updates and improve updated_journals

--- update_journals.py
+++ update_journals.py
@@ -15,19 +15,33 @@ for entry in bib_database.entries:
              )
         paper = ads.SearchQuery(doi=entry['doi'])
         paper.execute()
-        print(paper.articles, paper.articles[0])
-        assert len(paper.articles) == 1
-        article = paper.articles[0]
-        entry_bibtex = bibtexparser.loads(article.bibtex).entries[0]
-        for key in ('journal', 'pages', 'eid', 'month', 'year', 'adsurl',
-                    'volume'):
-            if key in entry_bibtex:
-                entry[key] = entry_bibtex[key]
-                print("Updated {0} to {1}".format(key, entry[key]))
     elif 'journal' in entry:
         print("Entry {0} is already complete".format(entry['ID']))
+        continue
+    elif 'eprint' in entry:
+        arxivid = entry['eprint'].split("v")[0]
+        paper = ads.SearchQuery(arXiv=arxivid)
+        paper.execute()
     else:
-        print("Could not search for entry: {0}".format(entry))
+        print("Could not search for entry: {0} because there is no journal or doi"
+              .format(entry))
+        continue
+
+    print(paper.articles, paper.articles[0])
+    assert len(paper.articles) == 1
+    article = paper.articles[0]
+    try:
+        entry_bibtex = bibtexparser.loads(article.bibtex).entries[0]
+    except Exception as ex:
+        print("Failed to parse {0} because of {1}"
+              .format(entry, ex))
+        continue
+
+    for key in ('journal', 'pages', 'eid', 'month', 'year', 'adsurl',
+                'volume'):
+        if key in entry_bibtex:
+            entry[key] = entry_bibtex[key]
+            print("Updated {0} to {1}".format(key, entry[key]))
 
 with open('cv.bib','w') as fh:
     bibtexparser.dump(bib_database, fh)
