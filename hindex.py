c069950647f487b5c0f4d7b77517e0c70991acd4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-01-10 11:18:02 -0700

    updated CV with some missing cites

--- hindex.py
+++ hindex.py
@@ -1,6 +1,7 @@
 import ads
 import datetime
 from astropy.utils.console import ProgressBar
+import bibtexparser
 from get_dev_key import get_dev_key
 
 print("Today is {0}".format(datetime.datetime.now().strftime("%D")))
@@ -29,8 +30,8 @@ citations = sorted([art.citation_count for art in ProgressBar(papers)])
 
 full_paper_info = [(art.first_author, art.first_author_norm,
                     art.pubdate, art.bibcode, art.id,
-                    #art.identifier,
-                    #art.author,
+                    art.identifier,
+                    art.author,
                     art.title, art.citation_count)
                    for art in ProgressBar(papers)]
 
@@ -49,3 +50,41 @@ with open('hindex.tex', 'w') as fh:
     fh.write("\\newcommand{{\\ntotalpapers}}{{{0}}}\n".format(len(mypapers.articles)))
     fh.write("\\newcommand{{\\ncites}}{{{0}}}\n".format(sum(citations)))
     fh.write("\\newcommand{{\\hindex}}{{{0}}}\n".format(h_index))
+
+
+
+
+with open('cv.bib','r') as fh:
+    bib_database = bibtexparser.load(fh)
+
+print("cv.bib matches:")
+matches = {}
+for entry in bib_database.entries:
+    doi = entry.get('doi')
+    bibcode = entry['adsurl'].split("/")[-1].replace("%26","&") if 'adsurl' in entry else None
+    match = [art for art in papers if doi in art.identifier or bibcode in art.identifier]
+    if len(match) == 0:
+        print("No match for {0}".format(entry.get('title')))
+    elif len(match) > 1:
+        print("Multiple matches for {0}".format(entry.get('title')))
+    else:
+        matches[bibcode] = match[0]
+        
+print()
+print("ADS search matches:")
+
+reverse_matches = {}
+for art in papers:
+    bibcode = art.bibcode
+
+    entry['adsurl'].split("/")[-1] if 'adsurl' in entry else None
+    match = [entry for entry in bib_database.entries
+             if any(doi in entry.get('doi',[]) for doi in art.identifier) or
+             bibcode in entry.get('adsurl', '').replace("%26","&")
+            ]
+    if len(match) == 0:
+        print("No match for {0}".format(art.title, art.bibcode))
+    elif len(match) > 1:
+        print("Multiple matches for {0}".format(art.title))
+    else:
+        reverse_matches[bibcode] = match[0]
